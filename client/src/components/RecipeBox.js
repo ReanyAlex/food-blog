195ec7bc59035568e8ff30f929cb7a38db8ec4c6
Alex Reany <reanyalex@gmail.com>
2017-09-20 13:48:31 -0700

    fixed image links

--- client/src/components/RecipeBox.js
+++ client/src/components/RecipeBox.js
@@ -16,12 +16,12 @@ export default ({ displayIndex, recipes }) => {
     <div>
       {recipesToBeDisplayed.map(recipe => {
         let imagePath = `/images/${recipe.title.toLowerCase().replace(/ /g, '_')}`;
-
+        console.log(process.env.PUBLIC_URL);
         return (
           <Box key={recipe._id}>
             <Link to={`/${recipe.title}/${recipe._id}`} className="recipe-link">
               <ImageWrapper>
-                <Image src={`./${imagePath}/${recipe.image}.jpg`} alt={recipe.title} />
+                <Image src={process.env.PUBLIC_URL + `${imagePath}/${recipe.image}.jpg`} alt={recipe.title} />
               </ImageWrapper>
               <Title>{recipe.title}</Title>
             </Link>
