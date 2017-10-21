5407dd339d0d99da5588f6fcfa3d7a34fe6ca5b2
Alex Reany <reanyalex@gmail.com>
2017-10-20 17:06:43 -0700

    move from landing to recipes 5sec

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 // styled-components keeped in a seperate file
 import { LandingContainer, ChineseHeader, EnglishHeader } from '../stylesheets/landingStyled';
 //for hover effect
 import '../stylesheets/landing.css';
 
-export default props => (
-  <div>
-    <LandingContainer>
-      <Link to="/recipes" className="landing-link">
-        <div className="landing-header-container">
-          <ChineseHeader id="landing_chinese">老外烹饪</ChineseHeader>
-          <EnglishHeader id="landing_english">American Cooks Chinese</EnglishHeader>
-        </div>
-      </Link>
-    </LandingContainer>
-  </div>
-);
+class Landing extends Component {
+  componentDidMount() {
+    this.sendToRecipes();
+  }
+
+  sendToRecipes() {
+    setTimeout(() => {
+      this.props.history.push('/recipes');
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div>
+        <LandingContainer>
+          <Link to="/recipes" className="landing-link">
+            <div className="landing-header-container">
+              <ChineseHeader id="landing_chinese">老外烹饪</ChineseHeader>
+              <EnglishHeader id="landing_english">American Cooks Chinese</EnglishHeader>
+            </div>
+          </Link>
+        </LandingContainer>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Landing);
